a6d7c8ab4c058c24b655d99c018f87bf4603a415
Rory Ward <R.Ward15@nuigalway.ie>
2020-10-22 17:14:10 +0100

    This is assignment 1 update1

--- src/main/java/ct417/rory_ward_assignment_1/Module.java
+++ src/main/java/ct417/rory_ward_assignment_1/Module.java
@@ -9,40 +9,39 @@ import java.util.*;
 
 public class Module {
     private String name;                 // Name
-    private long id;                     // ID
     private List<Student> students;      // students
-    private List courses;                // courses
+    private List<Course> courses;        // courses
     
     // construct a new module with given fields
-    public Module(String name, int age, String dob, long id, List students, List courses) {
+    public Module(String name) {
         this.name   = name;
-        this.id = id;
-        this.students = students;
-        this.courses = courses;
+        students = new ArrayList<>();
+        courses = new ArrayList<>();
     }
     
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public void addStudent(Student student) {
+        if(!this.students.contains(student)){
+            this.students.add(student);
+            student.addModule(this);
+        }
     }
 
-    public void setStudents(List students) {
-        this.students = students;
-    }
-
-    public void setCourses(List courses) {
-        this.courses = courses;
+    public void addCourse(Course course) {
+        if(!this.courses.contains(course)){
+            this.courses.add(course);
+            course.addModule(this);
+            for(Student student : this.students){
+                student.addCourse(course);
+            }
+        }
     }
 
     public String getName() {
         return name;
-    }
-
-    public long getId() {
-        return id;
     }
 
     public List getStudents() {
